e2dd7328845a7819c93f34c1c47f76227c382b58
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-11-21 17:02:36 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+{
+  const tasks = [
+    {
+      content: " nagrać lekcje",
+      done: false,
+    },
+    {
+      content: "zjeść pierogi",
+      done: true,
+    },
+  ];
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+      <li>
+        ${task.content}
+      </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
+  };
+  init();
+}
