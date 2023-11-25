c713c0631ae900580af4e4cdc2f392826e7c1f95
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-11-25 20:32:49 +0200

    line through mistake fixed

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@
         <button class="tasks__button tasks__button--toggleDone js-toggleDone">
         ${task.done ? "✔" : ""}
         </button>
-        <span class="tasks__content${
+        <span class="tasks__content ${
           task.done ? "tasks__content--done" : ""
         }">${task.content}</span>
         <button class="tasks__button tasks__button--remove js-remove">
