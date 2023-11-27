34db7518f4666ae725a7cbf692a1243ec5d071af
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-11-27 21:59:29 +0200

    final working version

--- js/script.js
+++ js/script.js
@@ -105,10 +105,10 @@
        if (tasks.length > 0) {
            buttonsElement.innerHTML = `
            <button class="task__hideOrShowAllDone js-toggleDoneTasks">
-           ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone zadania
+           ${hideDoneTasks ? "Show" : "Hide"} finished tasks
            </button>
            <button class="task__markAllDone js-markAllDone" ${tasks.every(({ done }) => done) ? "disabled" : ""}>
-           Oznacz wszystkie zadania jako ukończone
+           Mark all tasks done
            </button>`;
        } else {
            buttonsElement.innerHTML = ``;
